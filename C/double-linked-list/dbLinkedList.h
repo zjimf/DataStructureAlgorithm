43d043313b1948b621f9ba49c9c6276795c39314
zjimf <jmjmjim1101@gmail.com>
2023-07-19 13:32:28 +0800

    Add comment

--- C/double-linked-list/dbLinkedList.h
+++ C/double-linked-list/dbLinkedList.h
@@ -8,14 +8,48 @@ struct node {
 };
 typedef struct node Node;
 
+/** @brief Appends a new node to the end of the list
+ *
+ * @param head Double pointer to the head of the list
+ * @param data The value store in Node
+ *
+ * @return Void.
+ */
 void Append(Node **head, int data);
 
+/** @brief Print all of the node data from head
+ *
+ * @param head Pointer to the head of the list
+ *
+ * @return Void.
+ */
 void PrintList(Node *head);
 
+/** @brief Print the num of the node in the list
+ *
+ * @param head Pointer to the head of the list
+ *
+ * @return The count of nodes
+ */
 int Count(Node *head);
 
+/** @brief Delete the target value node in the list
+ *
+ * @param head Pointer to the head of the list
+ * @param target The value we want delete
+ *
+ * @return Void.
+ */
 void Delete(Node **head, int target);
 
+/** @brief Insert the node in the list
+ *
+ * @param head Double pointer to the head of the list
+ * @param data The node data
+ * @param index The index we want insert in the list
+ *
+ * @return Void.
+ */
 void Insert(Node **head, int data, int index);
 
 #endif
