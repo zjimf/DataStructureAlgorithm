7ece94697927b42e62426ea39609da87a38cfdd4
zjimf <jmjmjim1101@gmail.com>
2023-07-21 14:50:58 +0800

    Add comment

--- C/Stack/Stack_Arr.h
+++ C/Stack/Stack_Arr.h
@@ -10,12 +10,37 @@ struct Stack_Arr {
 
 typedef struct Stack_Arr Stack_Arr;
 
+/** @brief Push data in Stack
+ *
+ * @param s Pointer to the Stack
+ * @param val The value push in Stack
+ *
+ * @return Void.
+ */
 void push_Arr(Stack_Arr *s, int val);
 
+/** @brief Pop data in Stack
+ *
+ * @param s Pointer to the Stack
+ *
+ * @return Int. top value in Stack
+ */
 int pop_Arr(Stack_Arr *s);
 
+/** @brief Determine the Stack is empty
+ *
+ * @param s Pointer to the Stack
+ *
+ * @return Bool. the Stack is empty
+ */
 bool empty_Arr(Stack_Arr *s);
 
+/** @brief Print all data in the Stack, from top to bottom
+ *
+ * @param s Pointer to the Stack
+ *
+ * @return Void.
+ */
 void print_Arr(Stack_Arr *s);
 
 #endif
--- C/Stack/Stack_LinkedList.h
+++ C/Stack/Stack_LinkedList.h
@@ -8,12 +8,37 @@ struct node {
 
 typedef struct node Node;
 
+/** @brief Push data in Stack
+ *
+ * @param head double pointer to the Stack head
+ * @param val The value push in Stack
+ *
+ * @return Void.
+ */
 void push_LinkedList(Node **head, int val);
 
+/** @brief Pop data in Stack
+ *
+ * @param head double pointer to the Stack head
+ *
+ * @return Int. top value in Stack
+ */
 int pop_LinkedList(Node **head);
 
+/** @brief Determine the Stack is empty
+ *
+ * @param head double pointer to the Stack head
+ *
+ * @return Bool. the Stack is empty
+ */
 bool empty_LinkedList(Node *head);
 
+/** @brief Print all data in the Stack, from top to bottom
+ *
+ * @param head double pointer to the Stack head
+ *
+ * @return Void.
+ */
 void print_LinkedList(Node *head);
 
 #endif
