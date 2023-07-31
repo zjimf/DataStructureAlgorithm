aa4034bf5760b3c1b781931127cb72b5e8af7925
zjimf <jmjmjim1101@gmail.com>
2023-07-31 20:46:38 +0800

    Leetcode 1.two-sum

---
+++ Leetcode/1.two-sum.cpp
@@ -0,0 +1,20 @@
+/** 1. Two_Sum
+ *  @brief Using unordered_map(hash)
+ *         if found (target-current) in the map return the pair index,
+ *         else store in map {current value, current index}
+ *
+ *  @time_complexity O(N)
+ */
+class Solution {
+public:
+    vector<int> twoSum(vector<int> &nums, int target) {
+        unordered_map<int, int> map;
+        for (int i = 0; i < nums.size(); i++) {
+            if (map.find(target - nums[i]) != map.end())
+                return {map[target - nums[i]], i};
+            // not found
+            map[nums[i]] = i;
+        }
+        return {};
+    }
+};
