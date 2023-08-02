258992549b49e59d32d87b932703ae56fef5180d
zjimf <jmjmjim1101@gmail.com>
2023-08-02 10:22:02 +0800

    Leetcode 977.squares-of-a-sorted-array

---
+++ Leetcode/977.squares-of-a-sorted-array.cpp
@@ -0,0 +1,16 @@
+/** 977. Squares of a Sorted Array
+ *  @brief Squares all element, stored in new vector, sorting
+ *
+ *  @Time complexity O(NlogN)
+ */
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int> &nums) {
+        vector<int> newVec;
+        for (auto &n : nums) {
+            newVec.push_back(n * n);
+        }
+        sort(newVec.begin(), newVec.end());
+        return newVec;
+    }
+};
