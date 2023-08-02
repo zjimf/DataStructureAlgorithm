11db49ecd3e945877355f95a9d59c218990908cb
zjimf <jmjmjim1101@gmail.com>
2023-08-02 13:53:39 +0800

    27. Remove Element

---
+++ Leetcode/27.remove-element.cpp
@@ -0,0 +1,15 @@
+/** 27. Remove Element
+ *  @brief build new array to store
+ *
+ *  @Time complexity O(N)
+ */
+class Solution {
+public:
+    int removeElement(vector<int> &nums, int val) {
+        int index = 0;
+        for (int i = 0; i < nums.size(); i++)
+            if (nums[i] != val)
+                nums[index++] = nums[i];
+        return index;
+    }
+};
