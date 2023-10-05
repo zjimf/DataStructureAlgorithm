a0571c5e91d15cdc6033ef629c2478b559fc2d8a
zjimf <jmjmjim1101@gmail.com>
2023-10-05 10:47:06 +0800

    Change the way of writing

--- Leetcode/1658.minimum-operations-to-reduce-x-to-zero.cpp
+++ Leetcode/1658.minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,8 +1,8 @@
 /** 1658. Minimum Operations to Reduce X to Zero
  *  @brief 因為找兩端太難設計，故換一種想法，在nums中找可以形成sum-x(target)之最長的subArray。
- *         類似雙指標，i做為右指標持續推進，而左指標紀錄在map中。
- *         遍歷整個nums，以i推進，每次紀錄<目前的sum,目前的i>，為了紀錄左邊方塊的大小。
- *         每次看sum-target是否存在map中，若有，表示符合subArray，無則表示需繼續。
+ *         雙指標，right做為右指標持續推進
+ *         遍歷整個nums，以right推進，若是目前curSum的值已經大於target為left前進的條件
+ *         得注意left不得超過right
  *
  *  @Time_complexity O(N)
  */
@@ -14,25 +14,24 @@ public:
         for (auto n : nums)
             target += n;
 
-        if (target == x)
+        target -= x;
+
+        if (target == 0)
             return n;
 
-        unordered_map<int, int> map;
-        map[0] = -1;
-        target -= x;
-        int sum = 0;
-        int res = INT_MIN;
+        int curSum = 0, left = 0, res = 0;
 
-        for (int i = 0; i < n; i++) {
-            sum += nums[i];
+        for (int right = 0; right < n; right++) {
+            curSum += nums[right];
 
-            if (map.count(sum - target)) {
-                res = max(res, i - map[sum - target]);
-            }
+            while (left <= right && curSum > target)
+                curSum -= nums[left++];
 
-            map[sum] = i;
+            if (curSum == target)
+                res = max(res, right - left + 1);
         }
-        return res == INT_MIN ? -1 : n - res;
+
+        return res ? n - res : -1;
     }
 };
 // @lc code=end
