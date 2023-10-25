dfa77e529cc8ae28655f4ceab2f8e02d9e7d7291
zjimf <jmjmjim1101@gmail.com>
2023-10-25 14:27:49 +0800

    Leetcode Leetcode/83.remove-duplicates-from-sorted-list.cpp

---
+++ Leetcode/83.remove-duplicates-from-sorted-list.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=83 lang=cpp
+ *
+ * [83] Remove Duplicates from Sorted List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *deleteDuplicates(ListNode *head) {
+        ListNode *cur = head;
+        while (cur && cur->next) {
+            if (cur->val == cur->next->val)
+                cur->next = cur->next->next;
+            else
+                cur = cur->next;
+        }
+        return head;
+    }
+};
+// @lc code=end
