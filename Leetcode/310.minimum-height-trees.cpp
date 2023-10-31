e8972aff71ed4026e1e772be6c3fb9a18a1bd2fb
zjimf <jmjmjim1101@gmail.com>
2023-10-31 13:46:05 +0800

    Leetcode 310.minimum-height-trees

--- Leetcode/310.minimum-height-trees.cpp
+++ Leetcode/310.minimum-height-trees.cpp
@@ -1,15 +1,47 @@
-/*
- * @lc app=leetcode id=310 lang=cpp
+/** 310. Minimum Height Trees
+ * @brief 想成衣服掉在地上，抓一個點拿起來的感覺，每輪去除所有leaf直到只剩一到兩個節點
  *
- * [310] Minimum Height Trees
+ * https://home.gamer.com.tw/artwork.php?sn=5478747
+ * Time_complexity O(V+2E)
  */
-
-// @lc code=start
 class Solution {
 public:
-    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        
+    vector<int> findMinHeightTrees(int n, vector<vector<int>> &edges) {
+        vector<vector<int>> adjacentList(n);
+        vector<int> inDegree(n, 0), ans;
+        // build adjacentList, inDegree table
+        for (auto &e : edges) {
+            adjacentList[e[0]].push_back(e[1]);
+            adjacentList[e[1]].push_back(e[0]);
+            inDegree[e[0]]++;
+            inDegree[e[1]]++;
+        }
+
+        // put first round leaf
+        queue<int> leafQueue;
+        for (int i = 0; i < n; i++) {
+            if (inDegree[i] == 1) {
+                leafQueue.push(i);
+                inDegree[i]--;
+            }
+        }
+
+        // delete leaf every round
+        while (!leafQueue.empty()) {
+            int qSize = leafQueue.size();
+            ans.clear();
+            for (int i = 0; i < qSize; i++) {
+                int curr = leafQueue.front();
+                leafQueue.pop();
+                ans.push_back(curr);
+                for (auto linked : adjacentList[curr]) {
+                    inDegree[linked]--;
+                    if (inDegree[linked] == 1)
+                        leafQueue.push(linked);
+                }
+            }
+        }
+        return ans;
     }
 };
 // @lc code=end
-
