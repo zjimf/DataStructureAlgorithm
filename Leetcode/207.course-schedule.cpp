4537fa7b64745a89ffacd398452f50baf7b62290
zjimf <jmjmjim1101@gmail.com>
2023-11-04 14:09:03 +0800

    Adding DFS solution

--- Leetcode/207.course-schedule.cpp
+++ Leetcode/207.course-schedule.cpp
@@ -1,3 +1,4 @@
+// BFS
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>> &prerequisites) {
@@ -5,16 +6,14 @@ public:
         vector<int> inDegrees(numCourses, 0);
 
         for (const auto &prereq : prerequisites) {
-            graph[prereq[1]].push_back(prereq[0]);
-            inDegrees[prereq[0]]++;
+            graph[prereq[0]].push_back(prereq[1]);
+            inDegrees[prereq[1]]++;
         }
 
         queue<int> q;
-        for (int i = 0; i < numCourses; i++) {
-            if (inDegrees[i] == 0) {
+        for (int i = 0; i < numCourses; i++)
+            if (inDegrees[i] == 0)
                 q.push(i);
-            }
-        }
 
         int coursesFinished = 0;
         while (!q.empty()) {
@@ -33,3 +32,46 @@ public:
         return coursesFinished == numCourses;
     }
 };
+
+// DFS
+// class Solution {
+// public:
+//     bool canFinish(int numCourses, vector<pair<int, int>> &prerequisites) {
+//         graph g = buildGraph(numCourses, prerequisites);
+//         vector<bool> todo(numCourses, false), done(numCourses, false);
+//         for (int i = 0; i < numCourses; i++) {
+//             if (!done[i] && !acyclic(g, todo, done, i)) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+
+// private:
+//     typedef vector<vector<int>> graph;
+
+//     graph buildGraph(int numCourses, vector<pair<int, int>> &prerequisites) {
+//         graph g(numCourses);
+//         for (auto p : prerequisites) {
+//             g[p.second].push_back(p.first);
+//         }
+//         return g;
+//     }
+
+//     bool acyclic(graph &g, vector<bool> &todo, vector<bool> &done, int node) {
+//         if (todo[node]) {
+//             return false;
+//         }
+//         if (done[node]) {
+//             return true;
+//         }
+//         todo[node] = done[node] = true;
+//         for (int v : g[node]) {
+//             if (!acyclic(g, todo, done, v)) {
+//                 return false;
+//             }
+//         }
+//         todo[node] = false;
+//         return true;
+//     }
+// };
